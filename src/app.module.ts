62ba69237b7ce9e1ddbae0c8eece4f61a02b6f50
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2023-07-07 16:01:03 +0800

    remove app from nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { EventEmitterModule } from '@nestjs/event-emitter';
-import { join } from 'path';
 import { ChatGateway } from './app.gateway';
 
 @Module({
   imports: [
     PrismaModule,
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'app', 'build'),
-    }),
+    // ServeStaticModule.forRoot({
+    //   rootPath: join(__dirname, '..', 'app', 'build'),
+    // }),
     EventEmitterModule.forRoot(),
   ],
   controllers: [AppController],
